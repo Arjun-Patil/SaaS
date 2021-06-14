d92ba1ee0210f535343d1ad97b3b022f20937f39
Arjun Patil <arjunshivaji100@gmail.com>
2021-06-14 19:19:35 +0530

    Add to_display_list method

--- L4-ActiveRecord/todo.rb
+++ L4-ActiveRecord/todo.rb
@@ -23,19 +23,23 @@ class Todo < ActiveRecord::Base
     where("due_date > ?", Date.today)
   end
 
+  def self.to_displayable_list
+    all.map { |todo| todo.to_displayable_string }
+  end
+
   def self.show_list
     puts "My Todo-list\n\n"
 
     puts "Overdue\n"
-    puts overdue.map { |todo| todo.to_displayable_string }
+    puts overdue.to_displayable_list
     puts "\n\n"
 
     puts "Due Today\n"
-    puts due_today.map { |todo| todo.to_displayable_string }
+    puts due_today.to_displayable_list
     puts "\n\n"
 
     puts "Due Later\n"
-    puts due_later.map { |todo| todo.to_displayable_string }
+    puts due_later.to_displayable_list
     puts "\n\n"
   end
 
